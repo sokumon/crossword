0a07e1eb989c4cec5d787fe478d201aebb7def25
adityabhosle-1 <adityapb2016@gmail.com>
2023-08-09 16:11:23 +0530

    first word comes

--- generate.py
+++ generate.py
@@ -8,23 +8,36 @@ max_col_size = 13
 data = pd.read_csv("useful.csv")
 grid = np.zeros((max_row_size, max_col_size)) 
 # grid[max_row_size][max_col_size] 
-# print(len(grid))
+print(grid)
 def choose_word():
     song_id = data['id'][random.randrange(0,data.shape[0])]
     row_data = data.iloc[song_id]
     return row_data
 
-def choose_first_pos():
+def choose_first_word():
     word_ori = random.choice(ori)
-    first_word = choose_word()
+    choosen_word = choose_word()
+    first_word = "`"
+    if choosen_word['track_word_count'] > 1:
+        first_word = choosen_word['track_intials']
+    else:
+        first_word = choosen_word['track_titles']
     x_pos = random.randrange(0,grid.shape[0]-len(first_word))
     y_pos = random.randrange(0,grid.shape[0]-len(first_word))
-    
+    place_on_grid(first_word,x_pos,y_pos,ori)
+        
+
 
 def place_on_grid(word,xpos,ypos,ori):
+    print(word)
     if ori == "down":
         for i in range(0,len(word)):
-            grid[xpos][ypos+i] = word[i]
+            if grid[xpos][ypos+i] == 0:
+                grid[xpos][ypos+i] = ord(word[i])
     else:
         for i in range(0,len(word)):
-            grid[xpos+i][ypos] = word[i]
+            if grid[xpos+1][ypos] == 0:
+                grid[xpos+i][ypos] = ord(word[i])
+
+choose_first_word()
+print(grid)
